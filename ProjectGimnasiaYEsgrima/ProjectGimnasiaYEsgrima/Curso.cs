9e0f136e3a9db0668f6d2411811e0db5c85a5abd
Pablo Barragán <pablo_barragan@hotmail.com>
2018-06-21 11:47:20 -0300

    Primera Confimación Desarrollo

---
+++ ProjectGimnasiaYEsgrima/ProjectGimnasiaYEsgrima/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGimnasiaYEsgrima
+{
+    public class Curso
+    {
+        int id_curso { get; set; }
+        string nombre { get; set; }
+        DateTime fecha_inicio { get; set; }
+        DateTime fecha_fin { get; set; }
+        List<Socio> lista_socios { get; set; }
+        List<Profesor>lista_profesor { get; set; }
+
+    }
+}
---
+++ ProjectGimnasiaYEsgrima/ProjectGimnasiaYEsgrima/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGimnasiaYEsgrima
+{
+    public abstract class Persona
+    {
+        public int id_persona { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int dni { get; set; }
+        public string direccion { get; set; }
+        DateTime fecha_nacimiento { get; set; }
+
+    }
+}
